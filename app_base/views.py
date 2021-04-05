3b7199450f68c435da1b369dad76a2c7eb431742
Joao Lucas <lucasbene86@gmail.com>
2021-04-05 17:51:41 -0300

    Adicionada uma tabela.

--- app_base/views.py
+++ app_base/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 
+from .models import DadoProduto
+
 
 def index(request):
-    return render(request, 'index.html')
+    dados_produtos = DadoProduto.objects.values()
+
+    print(dados_produtos)
+    produtos = {
+        'nome':dados_produtos,
+    }
+
+
+    return render(request, 'index.html', produtos)
 
 
 def infor(request):
